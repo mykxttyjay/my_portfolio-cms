a5d9f85ecafdb7d1e63d8c16cbe85321d5214e7a
mykxttyjay <sabidoangel.uc@gmail.com>
2026-05-22 09:17:35 +0800

    Redeploy without build cache

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
