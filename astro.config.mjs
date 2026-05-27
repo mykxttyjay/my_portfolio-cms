f90f0b99f109f5c7a90a1315876bd63990c876ee
mykxttyjay <sabidoangel.uc@gmail.com>
2026-05-27 13:06:41 +0800

    modified the thumbnail preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://angelmariesabido.vercel.app/', // Replace with your actual domain
 });
