cec643227fb6a8de27f854d50a1f4c8234115458
mykxttyjay <sabidoangel.uc@gmail.com>
2026-05-28 10:39:53 +0800

    defineConfig is not defined

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+import react from '@astrojs/react';
+import emdash from 'emdash/astro';
+
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
