4633d28613bdd2d5a6c44eaff3d3ecb44d563085
mykxttyjay <sabidoangel.uc@gmail.com>
2026-05-28 11:03:48 +0800

    changed the server content

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-import http from 'node:http';
-import handler from './dist/server/entry.mjs';
-
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-  handler(req, res);
-});
-
-server.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
-});
+import './dist/server/entry.mjs';
